2788b54201a9d8dbc911546f5dac89c1c0614a7e
Kavindu Geethsara <72646448+wgkgwijerathne@users.noreply.github.com>
2023-10-09 22:27:36 +0530

    Updating User model

---
+++ folder01/api/models/User.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+
+const UserSchema = new mongoose.Schema(
+    {
+        username: {
+            type: String,
+            required: true, // Keep this property to make the field required
+            // unique: true,
+        },
+        email: {
+            type: String,
+            required: true, // Keep this property to make the field required
+            // unique: true,
+        },
+        password: {
+            type: String,
+            required: true, // Keep this property to make the field required
+        },
+        profilePic: {
+            type: String,
+            default: "",
+        },
+    },
+    { timestamps: true }
+);
+
+module.exports = mongoose.model("User", UserSchema);
