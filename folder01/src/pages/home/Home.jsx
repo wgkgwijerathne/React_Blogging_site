33a77b19b8aeead615aed38e0ddbfa83cde18fbe
Kavindu Geethsara <72646448+wgkgwijerathne@users.noreply.github.com>
2023-10-17 23:26:02 +0530

    Home Updated
    with axios

--- folder01/src/pages/home/Home.jsx
+++ folder01/src/pages/home/Home.jsx
@@ -2,8 +2,32 @@ import Header from "../../header/Header"
 import "./home.css"
 import Posts from "../../componets/posts/Posts"
 import Sidebar from "../../componets/sidebar/Sidebar"
+import { useEffect } from "react";
+import React, { useState } from 'react';
+import axios from 'axios';
+
+
 
 export default function Home() {
+  
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      try {
+        const res = await axios.get("/posts");
+        // Assuming the response data is an array of posts, you can update the state like this:
+        setPosts(res.data);
+        console.log(res);
+      } catch (error) {
+        // Handle any errors here, e.g., show an error message.
+        console.error("Error fetching posts:",error);
+      }
+    };
+
+    fetchPosts();
+  }, []);
+
   return (
     <>
     <Header />
