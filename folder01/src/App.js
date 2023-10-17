258ddda5c07d9d13385e5ecefb84a90b53cf687b
Kavindu Geethsara <72646448+wgkgwijerathne@users.noreply.github.com>
2023-10-17 23:29:17 +0530

    Updated

--- folder01/src/App.js
+++ folder01/src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import TopBar from "./componets/topbar/TopBar";
+import Home from "./pages/home/Home";
+import Single from "./pages/single/Single";
+import Write from "./pages/write/Write";
+import Setting from "./pages/setting/Setting"
+import Login from "./pages/login/Login";
+import Register from "./pages/register/Register";
+import { BrowserRouter as Router, Route, Link, Routes } from "react-router-dom";
+
 
 function App() {
+
+  const user = false;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Router>
+    <TopBar/>
+
+    <Routes>
+      <Route exact path="/" element={<Home/>}/>
+      <Route path="/register" element={user ? <Home/> : <Register/>}/>
+      <Route path="/login" element={user ? <Home/> : <Login/>}/>
+      <Route path="/write" element={user ? <Write/> : <Register/>}/>
+      <Route path="/setting" element={user ? <Setting/> : <Register/>}/>
+    
+      
+    </Routes>
+
+  </Router>
+
+  ); 
 }
 
 export default App;
