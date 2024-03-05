1f7536c10a11efd5d9a438a237c77745906c90a5
Kavindu Geethsara <72646448+wgkgwijerathne@users.noreply.github.com>
2024-03-05 09:47:22 +0530

    Updated users

--- folder01/api/models/User.js
+++ folder01/api/models/User.js
@@ -5,12 +5,12 @@ const UserSchema = new mongoose.Schema(
         username: {
             type: String,
             required: true, // Keep this property to make the field required
-            // unique: true,
+            unique: true,
         },
         email: {
             type: String,
             required: true, // Keep this property to make the field required
-            // unique: true,
+            unique: true,
         },
         password: {
             type: String,
